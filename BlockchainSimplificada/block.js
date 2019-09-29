85f8676d40526d95d5661227e2b765d7c7c346eb
illiamw <33575357+illiamw@users.noreply.github.com>
2019-09-29 19:44:12 -0300

    Replace code Blockchain Simplifity

---
+++ BlockchainSimplificada/block.js
@@ -0,0 +1,38 @@
+/**
+ * @var crypto Variavel Global com os metodos de criptografia.
+ */
+const crypto = require('./node_modules/crypto-js');
+const chain = require('./chain');
+/**
+ * @module generateHash geração de hash criptografado
+ */
+module.exports.generateHash = function({index, prevHash, timestamp, data}) {
+  return crypto.SHA256(data + index + prevHash + timestamp).toString();;
+}
+
+module.exports.create = (data) => {
+  const lastBlock = chain.last();
+  
+  const newblock = {
+    index : lastBlock.index + 1,
+    prevHash : lastBlock.hash,
+    timestamp : new Date().getTime(),
+    data : data,
+  }
+  newblock.hash = this.generateHash(newblock);
+  return newblock;
+}
+
+module.exports.validateBlock = (newBlock, lastBlock = chain.last()) => {
+  let blockIsValid = false;
+
+  if (newBlock.index == lastBlock.index+1) {
+    blockIsValid = true;
+  } else if (newBlock.prevHash == lastBlock.hash) {
+    blockIsValid = true;
+  } else if (newBlock.hash == this.generateHash(newBlock)) {
+    blockIsValid = true
+  }
+
+  return blockIsValid;
+}
---
+++ BlockchainSimplificada/chain.js
@@ -0,0 +1,86 @@
+/**
+ * Creditos a  @author Jorge luís
+ * Modificada por @author William Luis Alves Ferreira
+ * Modificado por @author Bruner Eduardo
+ */
+const block = require('./block');
+/**
+ * @description Função instanciadora do lista de blocos da chain, inicialidando com bloco inicial nulo
+ */
+const Chain = (function() {
+  let instance;
+
+  const firstBlock = {
+    index : 0,
+    prevHash : 0,
+    timestamp : 0,
+    data : 'Blockchain is Borning',
+    hash : block.generateHash({index : 0, prevHash : 0, timestamp : 0,  data : 'Blockchain is Borning'}),
+  };
+
+  const chain = [firstBlock];
+/**
+ * @description Valida o update da chain usando como parametro o tempo de inserção e coerência com o restante da rede
+ * @param {Object} chain bloco inicial 
+ */
+  function validateChain(chain) {
+    let validChain = true;
+    if(JSON.stringify(chain[0]) !== JSON.stringify(origin)) {
+      validChain = false;
+      return validChain;
+    }
+    
+    const temp = [chain[0]];
+
+    for(let i=1; i < chain.length; i++) {
+      if (block.validateBlock(chain[i], temp[i-1])) {
+        temp.push(chain[i]);
+      } else {
+        validChain = false;
+        return validChain;
+      }
+    }
+
+    return validChain;
+
+  }
+
+  function get() {
+    return chain;
+  };
+
+  function last() {
+    return chain.slice().pop();
+  }
+
+  function update(newBlock) {
+    block.validateBlock(newBlock) ? chain.push(newBlock) : console.log('invalid block recived');
+  }
+
+  function replace(newChain) {
+    if (validateChain(newChain) && newChain.length > chain.length) {
+      chain.splice(0, chain.length);
+      chain.push(...newChain);
+    } else {
+      console.log('invalid chain recived');
+    }
+  }
+
+  function create() {
+    return {get, last, update, replace}
+  }
+
+  return {
+    init() {
+      if (!instance) {
+        instance = create();
+      }
+
+      return instance;
+
+    }
+  };
+
+})();
+
+module.exports = Chain.init();
